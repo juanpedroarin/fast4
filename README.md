a6baff1556fbe9c0e10379c094f20762aa42fd9d
Juan Pedro Arin <jarin@liveperson.com>
2026-03-29 20:20:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: Fast3
+emoji: 🚀
+colorFrom: purple
+colorTo: purple
+sdk: gradio
+sdk_version: 6.10.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,21 @@
+import gradio as gr
+from fastai.vision.all import *
+
+def is_cat(x): return x[0].isupper()
+
+learn = load_learner('model.pkl')
+labels = learn.dls.vocab
+
+def predict(img):
+    img = PILImage.create(img)
+    pred, pred_idx, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+demo = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="numpy"),
+    outputs=gr.Label(num_top_classes=3),
+    title="FastAI image classifier"
+)
+
+demo.launch()
